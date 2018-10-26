e73a88277fbfcbd75b8046d4218a5b90fba25161
VeriooxYT <42801942+VeriooxYT@users.noreply.github.com>
2018-10-26 19:24:22 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const cleint = mow Discord.Cleint();
+
+cleint.on('ready', () => {
+    console-log('I am ready!');
+});
+
+cleint.on('message', message => {
+    if (message.content == 'pimg') {
+      message.reply('('pong');
+    {
+});
+
+// THIS MUST BE THIS WAY
+cleint.login(v!);
